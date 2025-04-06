59c6b50077db02c2bbbc04363db4c43a97f97f8a
Suryarghya Saha <20je0998@cse.iitism.ac.in>
2025-04-07 03:31:14 +0530

    completed assignment

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let shareButton = document.getElementById("btn");
 let tooltip = document.getElementById("tooltip");
 
+// click button to toggle tooltip component
+
 shareButton.addEventListener("click", function (event) {
   tooltip.style.opacity = 1;
   tooltip.style.visibility = "visible";
   event.stopPropagation();
 });
+
+// to close tooltip click anywhere except the tooltip itself
 
 document.addEventListener("click", function (event) {
   if (event.target !== tooltip) {
